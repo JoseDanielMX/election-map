ecfed7fe23fd52ad6bc919dbd7293bf1e49ffda5
JoseDanielMX <jduvmx@gmail.com>
2020-09-25 19:23:38 -0500

    Added comments for better organization and a function for tallying votes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Creating multiple objects
 var createPolitician = function(name) {
     var politician = {};
     politician.name = name;
@@ -10,6 +11,7 @@ var createPolitician = function(name) {
 var john = createPolitician("John");
 var jane = createPolitician("Jane");
 
+// Assigning election results
 john.electionResults = [5, 1, 7, 2, 33, 6, 4 , 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 jane.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
@@ -21,3 +23,19 @@ jane.electionResults[4] = 38;
 
 john.electionResults[43] = 11;
 jane.electionResults[43] = 27;
+
+// Tallying votes
+politician.tallyUpTotalVotes = function() {
+    this.totalVotes = 0;
+    for (var i = 0; i < this.electionResults.length; i++) {
+        this.totalVotes = this.totalVotes + this.electionResults[i];
+    }
+}
+
+// Calling the method for each politician
+john.tallyUpTotalVotes();
+jane.tallyUpTotalVotes();
+
+// Consoling log total votes
+console.log(john.totalVotes);
+console.log(jane.totalVotes);
